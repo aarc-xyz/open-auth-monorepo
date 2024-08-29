1ec2365c107493f01b37beaeacc43fbd55ed270d
vrajdesai78 <vrajdesai78@gmail.com>
2024-08-29 17:26:49 +0530

    updated config

--- packages/auth-widget/src/App.tsx
+++ packages/auth-widget/src/App.tsx
@@ -1,45 +1,52 @@
-import './App.css'
-import { Button } from 'antd'
-import { useState } from 'react'
-import useAuthWidget from './hooks/useAuthWidget'
-import { AarcAuthWidgetConfig, AuthMethod, OAuthProvider } from './components/types.ts'
-import Provider from './provider.tsx'
-import { useWallet } from './hooks/useWallet.tsx'
+import "./App.css";
+import { Button } from "antd";
+import { useState } from "react";
+import useAuthWidget from "./hooks/useAuthWidget";
+import {
+  AarcAuthWidgetConfig,
+  AuthMethod,
+  OAuthProvider,
+} from "./components/types.ts";
+import Provider from "./provider.tsx";
+import { useWallet } from "./hooks/useWallet.tsx";
 
 // App component for development and testing of the AuthWidget
 function App() {
-
-  const [data, setData] = useState<any>(null)
+  const [data, setData] = useState<any>(null);
 
   const config: AarcAuthWidgetConfig = {
-
     Wallet: function Wallet() {
-      return <div></div>
+      return <div></div>;
     },
     callbacks: {
       onSuccess: (data: any) => {
-        setData(data)
+        setData(data);
       },
       onError: (data: any) => {
-        console.log("onError", data)
+        console.log("onError", data);
       },
       onClose: (data: any) => {
-        console.log("onClose", data)
+        console.log("onClose", data);
       },
       onOpen: (data: any) => {
-        console.log("onOpen", data)
-      }
+        console.log("onOpen", data);
+      },
     },
     appearance: {
       logoUrl: "https://dashboard.aarc.xyz/assets/AuthScreenLogo-CNfNjJ82.svg",
       themeColor: "#FF0000",
       textColor: "blue",
-      darkMode: false
+      darkMode: false,
     },
     authMethods: [AuthMethod.WALLET, AuthMethod.SMS, AuthMethod.EMAIL],
-    socialAuth: [OAuthProvider.TELEGRAM, OAuthProvider.FARCASTER, OAuthProvider.TWITTER, OAuthProvider.GOOGLE,],
-    aarcApiKey: 'aarc_api_key',
+    socialAuth: [
+      OAuthProvider.TELEGRAM,
+      OAuthProvider.FARCASTER,
+      OAuthProvider.TWITTER,
+      OAuthProvider.GOOGLE,
+    ],
     chainId: 11155111,
+    aarcApiKey: "294ffbcf-6a16-4e8a-8b5c-9aca09188f36",
     urls: {
       stytchUrls: {
         prod: "https://api.stytch.aarc.xyz/",
@@ -47,55 +54,50 @@ function App() {
       },
       pollUrls: {
         prod: "https://open-auth.aarc.xyz/",
-        staging: "https://open-auth.staging.aarc.xyz/",
+        staging: "http://127.0.0.1:4002/",
       },
       publicToken: {
         prod: "stytch-public-prod-token",
-        staging: "stytch-public-stage-token",
+        staging: "public-token-test-462fe37f-340b-456a-8dfc-6cc61d8bdadf",
       },
       redirectUrl: {
         prod: "https://auth.aarc.network/",
-        staging: "https://auth-staging.aarc.network/"
-      }
+        staging: "https://auth.aarc.network/",
+      },
     },
-    env: 'staging'
-  }
+    env: "staging",
+  };
 
   return (
     <div>
       <Provider config={config}>
         <Auth data={data} config={config} />
       </Provider>
-
     </div>
-  )
+  );
 }
 
-function Auth({ data, config }: { data: any, config: any }) {
-  const { openAuthWidget } = useAuthWidget()
-  const sendTransaction = useWallet(config)
+function Auth({ data, config }: { data: any; config: any }) {
+  const { openAuthWidget } = useAuthWidget();
+  const sendTransaction = useWallet(config);
   const handleSendTransaction = async () => {
-
     sendTransaction(
       {
-        from: 'fromWalletAddress',
-        to: 'toWalletAddress',
-        value: 2
+        from: "fromWalletAddress",
+        to: "toWalletAddress",
+        value: 2,
       },
       80001,
       config.aarcApiKey
-    )
-
-  }
+    );
+  };
   return (
     <div>
       <p>{JSON.stringify(data)}</p>
-      <Button
-        onClick={openAuthWidget}
-      >Open</Button>
+      <Button onClick={openAuthWidget}>Open</Button>
       <Button onClick={() => handleSendTransaction()}>Send Transaction</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
